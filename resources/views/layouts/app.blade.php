c637b92e370ff27068bb1e0e91e3bd1514cbf926
Min Khant Maw <mr.minkhantmaw@gmail.com>
2023-02-23 22:22:01 +0630

    ui fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,9 @@
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;700&display=swap"
-        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400&display=swap" rel="stylesheet">
     <!-- MDB -->
 
 
@@ -37,9 +38,7 @@
 
 <body>
     <div class="page-wrapper chiller-theme toggled">
-        <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-            <i class="fas fa-bars"></i>
-        </a>
+
         <nav id="sidebar" class="sidebar-wrapper">
             <div class="sidebar-content">
                 <div class="sidebar-brand">
@@ -237,114 +236,18 @@
             </div>
         </nav>
         <!-- sidebar-wrapper  -->
-        <main class="page-content">
-            <div class="container-fluid">
-                <h2>Pro Sidebar</h2>
-                <hr>
-                <div class="row">
-                    <div class="form-group col-md-12">
-                        <p>This is a responsive sidebar template with dropdown menu based on bootstrap 4 framework.</p>
-                        <p> You can find the complete code on <a
-                                href="https://github.com/azouaoui-med/pro-sidebar-template" target="_blank">
-                                Github</a>, it contains more themes and background image option</p>
-                    </div>
-                    <div class="form-group col-md-12">
-                        <iframe
-                            src="https://ghbtns.com/github-btn.html?user=azouaoui-med&repo=pro-sidebar-template&type=star&count=true&size=small"
-                            frameborder="0" scrolling="0" width="90px" height="30px"></iframe>
-                        <iframe
-                            src="https://ghbtns.com/github-btn.html?user=azouaoui-med&repo=pro-sidebar-template&type=fork&count=true&size=small"
-                            frameborder="0" scrolling="0" width="90px" height="30px"></iframe>
-                    </div>
-                    <div class="form-group col-md-12">
-                        <div class="alert alert-success" role="alert">
-                            <h4 class="alert-heading">New !</h4>
-                            <p>New react pro sidebar library is now available on <a
-                                    href="https://www.npmjs.com/package/react-pro-sidebar" target="_blank">npm</a> <a
-                                    href="https://github.com/azouaoui-med/react-pro-sidebar" target="_blank">
-                                    <img alt="GitHub stars"
-                                        src="https://img.shields.io/github/stars/azouaoui-med/react-pro-sidebar?style=social" />
-                                </a></p>
-                            <a href="https://github.com/azouaoui-med/react-pro-sidebar" target="_blank"
-                                class="btn btn-sm btn-primary mr-2">
-                                Github</a>
-                            <a href="https://azouaoui-med.github.io/react-pro-sidebar" target="_blank"
-                                class="btn btn-sm btn-success">
-                                Demo</a>
-
-                        </div>
-
-                    </div>
-                </div>
-                <h5>More templates</h5>
-                <hr>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-                        <div class="card rounded-0 p-0 shadow-sm">
-                            <img src="https://user-images.githubusercontent.com/25878302/58369568-a49b2480-7efc-11e9-9ca9-2be44afacda1.png"
-                                class="card-img-top rounded-0" alt="Angular pro sidebar">
-                            <div class="card-body text-center">
-                                <h6 class="card-title">Angular Pro Sidebar</h6>
-                                <a href="https://github.com/azouaoui-med/angular-pro-sidebar" target="_blank"
-                                    class="btn btn-primary btn-sm">Github</a>
-                                <a href="https://azouaoui-med.github.io/angular-pro-sidebar/demo/" target="_blank"
-                                    class="btn btn-success btn-sm">Preview</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-                        <div class="card rounded-0 p-0 shadow-sm">
-                            <img src="https://user-images.githubusercontent.com/25878302/58369258-33f20900-7ef8-11e9-8ff3-b277cb7ed7b4.PNG"
-                                class="card-img-top rounded-0" alt="Angular pro sidebar">
-                            <div class="card-body text-center">
-                                <h6 class="card-title">Angular Dashboard</h6>
-                                <a href="https://github.com/azouaoui-med/lightning-admin-angular" target="_blank"
-                                    class="btn btn-primary btn-sm">Github</a>
-                                <a href="https://azouaoui-med.github.io/lightning-admin-angular/demo/" target="_blank"
-                                    class="btn btn-success btn-sm">Preview</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr>
-
-                <footer class="text-center">
-                    <div class="mb-2">
-                        <small>
-                            © 2020 made with <i class="fa fa-heart" style="color:red"></i> by - <a target="_blank"
-                                rel="noopener noreferrer" href="https://azouaoui.netlify.com">
-                                Mohamed Azouaoui
-                            </a>
-                        </small>
-                    </div>
-                    <div>
-                        <a href="https://github.com/azouaoui-med" target="_blank">
-                            <img alt="GitHub followers"
-                                src="https://img.shields.io/github/followers/azouaoui-med?label=github&style=social" />
-                        </a>
-                        <a href="https://twitter.com/azouaoui_med" target="_blank">
-                            <img alt="Twitter Follow"
-                                src="https://img.shields.io/twitter/follow/azouaoui_med?label=twitter&style=social" />
-                        </a>
-                    </div>
-                </footer>
-            </div>
-        </main>
-        <!-- page-content" -->
-    </div>
-    <div id="app">
         <div class="header-menu">
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="d-flex justify-content-between">
-                        <a href=""></a>
+                        <a href="" id="show-sidebar"><i class="fas fa-bars"></i></a>
                         <h5> @yield('title')</h5>
                         <a href=""></a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="">
+        <div class="py-4">
             @yield('content')
             </d>
         </div>
@@ -372,51 +275,53 @@
                 </div>
             </div>
         </div>
-        <!-- JQuery -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </div>
 
-        <!-- Bootstrap tooltips -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-        <!-- Bootstrap core JavaScript -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js">
-        </script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
 
-        <!-- MDB core JavaScript -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
-        <script>
-            jQuery(function($) {
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js">
+    </script>
 
-                $(".sidebar-dropdown > a").click(function() {
-                    $(".sidebar-submenu").slideUp(200);
-                    if (
-                        $(this)
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+    <script>
+        jQuery(function($) {
+
+            $(".sidebar-dropdown > a").click(function() {
+                $(".sidebar-submenu").slideUp(200);
+                if (
+                    $(this)
+                    .parent()
+                    .hasClass("active")
+                ) {
+                    $(".sidebar-dropdown").removeClass("active");
+                    $(this)
                         .parent()
-                        .hasClass("active")
-                    ) {
-                        $(".sidebar-dropdown").removeClass("active");
-                        $(this)
-                            .parent()
-                            .removeClass("active");
-                    } else {
-                        $(".sidebar-dropdown").removeClass("active");
-                        $(this)
-                            .next(".sidebar-submenu")
-                            .slideDown(200);
-                        $(this)
-                            .parent()
-                            .addClass("active");
-                    }
-                });
-
-                $("#close-sidebar").click(function() {
-                    $(".page-wrapper").removeClass("toggled");
-                });
-                $("#show-sidebar").click(function() {
-                    $(".page-wrapper").addClass("toggled");
-                });
+                        .removeClass("active");
+                } else {
+                    $(".sidebar-dropdown").removeClass("active");
+                    $(this)
+                        .next(".sidebar-submenu")
+                        .slideDown(200);
+                    $(this)
+                        .parent()
+                        .addClass("active");
+                }
             });
-        </script>
+
+            $("#close-sidebar").click(function() {
+                $(".page-wrapper").removeClass("toggled");
+            });
+            $("#show-sidebar").click(function() {
+                $(".page-wrapper").addClass("toggled");
+            });
+        });
+    </script>
 </body>
 
 </html>
